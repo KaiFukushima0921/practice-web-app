8cd66a1581b84393437bb973e93ba3141393311b
KaiFukushima0921 <kai2co2@gmail.com>
2023-10-29 12:42:05 +0900

    初めてのGitHub

---
+++ function.py
@@ -0,0 +1,22 @@
+# l = [1,2,3]
+# s1 = sum(l)
+# print(s1)
+
+# p= pow(2,8)
+# print(p)
+
+
+
+# def calc_double(n):
+#     return n*2
+
+# data1 = [1,3,5,10]
+
+# data2 = map(calc_double, data1)
+# print(list(data2))
+
+
+data1 = [1,2,3]
+data2 = ['a', 'b', 'c', 'd']
+for x, y in zip(data1, data2):
+    print(x, y)
